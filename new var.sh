c20a5c288e4068814667fdbd4ad3b9ea225d95ab
vinay <vinaythativati@gmail.com>
2026-05-22 23:00:06 +0530

    gg

---
+++ new var.sh
@@ -0,0 +1,13 @@
+name=vinay
+age = 27
+
+echo "my name is $name and my age is $age"
+
+
+student1=$1
+student2=$2
+echo "my name is $student1 and my age is $student2"
+
+read username
+read -s password
+echo "this is my username $username and this is my password $password"
