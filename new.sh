ba90e623af0577288a5c97be6e5566264e51f804
vinay <vinaythativati@gmail.com>
2026-05-22 08:09:00 +0000

    commited

--- new.sh
+++ new.sh
@@ -3,9 +3,9 @@
 person1=russia
 person2=china
 
-echo '$person1: hey iran this is my warning'
-echo '$person2: iam ready to face'
-echo '$person1: be prepare for war'
-echo '$person2: i m ok you as well'
-echo '$person1: let start war'
-echo '$person2: let c who will win'
+echo $person1: 'hey iran this is my warning'
+echo $person2: 'iam ready to face'
+echo $person1: 'be prepare for war'
+echo $person2: 'i m ok you as well'
+echo $person1: 'let start war'
+echo $person2: 'let c who will win'
