387c7893ebf38ffe354efb2fe5bf305581e7bfe2
vinay <vinaythativati@gmail.com>
2026-05-22 08:04:59 +0000

    nwe

--- new.sh
+++ new.sh
@@ -2,7 +2,7 @@
 
 echo 'Trump: hey iran this is my warning'
 echo 'iran: iam ready to face'
-echo 'Trump: 'be prepare for war'
+echo 'Trump: be prepare for war'
 echo 'iran: i m ok you as well'
 echo 'Trump: let start war'
 echo 'iran: let c who will win'
