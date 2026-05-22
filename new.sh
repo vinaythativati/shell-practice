8a39b0a29f55c4452de74d8f88ce9dabbd1ac007
vinay <vinaythativati@gmail.com>
2026-05-22 07:50:20 +0000

    conversion

---
+++ new.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+Trump: 'hey iran this is my warning'
+iran: 'iam ready to face'
+Trump: 'be prepare for war'
+iran: 'i m ok you as well'
+Trump: 'let start war'
+iran: 'let c who will win'
