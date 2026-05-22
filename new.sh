4f2248bb5838b24ee692bfc7c1f7774d73aa8329
vinay <vinaythativati@gmail.com>
2026-05-22 08:03:51 +0000

    new1

--- new.sh
+++ new.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-Trump: echo 'hey iran this is my warning'
-iran: echo 'iam ready to face'
-Trump: echo 'be prepare for war'
-iran: echo 'i m ok you as well'
-Trump: echo 'let start war'
-iran: echo 'let c who will win'
+echo 'Trump: hey iran this is my warning'
+echo 'iran: iam ready to face'
+echo 'Trump: 'be prepare for war'
+echo 'iran: i m ok you as well'
+echo 'Trump: let start war'
+echo 'iran: let c who will win'
