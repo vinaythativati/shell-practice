c5b4a0df6827f98d613fbb86fe1334b69927f57f
vinay <vinaythativati@gmail.com>
2026-05-22 23:13:07 +0530

    f

--- date.sh
+++ date.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 time=$(date)
 echo "current date is $time"
+
+store=$(date +%s)
+
+sleep 5
+
+end =$(date +%s)
+diff=$((end-store))
+echo "time taken to execute the script is $diff seconds"
