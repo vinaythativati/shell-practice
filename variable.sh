6f2ab99a425546db227af0c5ba99c25d1e03d3de
vinay <vinaythativati@gmail.com>
2026-05-22 22:30:08 +0530

    new

---
+++ variable.sh
@@ -0,0 +1,10 @@
+
+person1=$1
+person2=$2
+
+echo $person1: 'hey $person2 this is my warning'
+echo $person2: 'iam ready to face'
+echo $person1: 'be prepare for war'
+echo $person2: 'i m ok you as well'
+echo $person1: 'let start war'
+echo $person2: 'let c who will win'
