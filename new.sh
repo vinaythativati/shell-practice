5104c778bfa998bf4a25301675f679e19f488401
vinay <vinaythativati@gmail.com>
2026-05-22 08:07:17 +0000

    commited

--- new.sh
+++ new.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-echo 'Trump: hey iran this is my warning'
-echo 'iran: iam ready to face'
-echo 'Trump: be prepare for war'
-echo 'iran: i m ok you as well'
-echo 'Trump: let start war'
-echo 'iran: let c who will win'
+person1=russia
+person2=china
+
+echo '$person1: hey iran this is my warning'
+echo '$person2: iam ready to face'
+echo '$person1: be prepare for war'
+echo '$person2: i m ok you as well'
+echo '$person1: let start war'
+echo '$person2: let c who will win'
