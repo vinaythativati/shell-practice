8844b274ce1e322b4b1da83dfae2142757bcdbc4
vinay <vinaythativati@gmail.com>
2026-05-24 20:39:46 +0530

    h

--- cmd.sh
+++ cmd.sh
@@ -7,6 +7,12 @@ if [ $user -ne 0 ]; then
     exit 1
 fi
 
+dnf list installed mysql 
+
+if [ $? -eq 0 ]; then
+    echo "mysql is already installed"
+fi
+
    #installing mysq
    echo "installing mysql"
 dnf install mysql -y
@@ -17,3 +23,4 @@ else
     echo "mysql installation failed"
     exit 1
 fi
+
