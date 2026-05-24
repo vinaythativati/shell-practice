256ef0594beb3a7a9232708e244cc350f579fe4a
vinay <vinaythativati@gmail.com>
2026-05-24 19:37:03 +0530

    h

--- conditions.sh
+++ conditions.sh
@@ -2,7 +2,7 @@
 number=$1
 if [ $number -gt 20 ]; then
     echo "given number greater than 20"
-    elif [ $number -eq 20]; then
+    elif [ $number -eq 20 ]; then
     echo "given number is equal to 20"
     else
     echo "given number is less than 20"
