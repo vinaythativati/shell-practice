d9d64830567c717dd45f0137b0a1b02eb17d5c55
vinay <vinaythativati@gmail.com>
2026-05-24 13:05:42 +0530

    spl

--- special.sh
+++ special.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 name=$@
 value=$#
+vname=$0
 first=$1
 second=$2
 echo "All variables passwd to script :$name"
 echo "no of variables passed value :$value"
+echo "print script name: $0"
 echo "print first varibale: $first"
 echo "print second variable: $second"
