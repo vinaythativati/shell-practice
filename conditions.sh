a43c30e95d2bce087c8bb6f87b8d17ddd37c6898
vinay <vinaythativati@gmail.com>
2026-05-24 19:40:33 +0530

    h

--- conditions.sh
+++ conditions.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 number=$1
 if [ $number -gt 20 ]; then
-    echo "given number greater than 20"
-    elif [ $number -eq 20 ]; then
-    echo "given number is equal to 20"
-    else
-    echo "given number is less than 20"
+    echo " $number is greater than 20"
+elif [ $number -eq 20 ]; then
+    echo "$number is equal to 20"
+else
+    echo "$number is less than 20"
 fi
-
