4c6d91b8606683d6716d7991201f22328cd06775
vinay <vinaythativati@gmail.com>
2026-05-24 13:03:56 +0530

    special

--- special.sh
+++ special.sh
@@ -2,6 +2,8 @@
 name=$@
 value=$#
 first=$1
+second=$2
 echo "All variables passwd to script :$name"
 echo "no of variables passed value :$value"
 echo "print first varibale: $first"
+echo "print second variable: $second"
